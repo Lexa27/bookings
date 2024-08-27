ceb56b668978df2dbecf79078195a6b0f37d2bb3
Lexa27 <forzee64@gmail.com>
2024-08-27 17:47:25 +0200

    Improved major's room page

---
+++ migrations/20240827154311_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('General''s Quarters','2024-08-22 00:00:00.000','2024-08-22 00:00:00.000'),
+	 ('Major''s Suite','2024-08-23 00:00:00.000','2024-08-23 00:00:00.000');
---
+++ migrations/20240827154545_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+	 ('Reservation','2024-08-23 00:00:00','2024-08-23 00:00:00'),
+	 ('Owner Block','2024-08-23 00:00:00','2024-08-23 00:00:00');

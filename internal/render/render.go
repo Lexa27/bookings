adc5e423d36a0ed63b4f2832d13fad90c19b8557
Lexa27 <forzee64@gmail.com>
2024-09-11 14:32:27 +0200

    Added listing all reservations

--- internal/render/render.go
+++ internal/render/render.go
@@ -8,13 +8,16 @@ import (
 	"log"
 	"net/http"
 	"path/filepath"
+	"time"
 
 	"github.com/Lexa27/bookings/internal/config"
 	"github.com/Lexa27/bookings/internal/models"
 	"github.com/justinas/nosurf"
 )
 
-var functions = template.FuncMap{}
+var functions = template.FuncMap{
+	"humanDate": HumanDate,
+}
 
 var app *config.AppConfig
 var pathToTemplates = "./templates"
@@ -22,6 +25,11 @@ var pathToTemplates = "./templates"
 // NewRenderer sets the config for the template package
 func NewRenderer(a *config.AppConfig) {
 	app = a
+}
+
+// HumanDate returns time in YYYY-Month-DD format
+func HumanDate(t time.Time) string {
+	return t.Format("2006-January-02")
 }
 
 func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {

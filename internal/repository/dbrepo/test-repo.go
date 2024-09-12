c94129a93e801016b3f7a625f976c429ba74f6f8
Lexa27 <forzee64@gmail.com>
2024-09-12 13:43:16 +0200

    Added updating and processing reservations

--- internal/repository/dbrepo/test-repo.go
+++ internal/repository/dbrepo/test-repo.go
@@ -83,3 +83,15 @@ func (m *testDBRepo) GetReservationByID(id int) (models.Reservation, error) {
 
 	return res, nil
 }
+
+func (m *testDBRepo) UpdateReservation(u models.Reservation) error {
+	return nil
+}
+
+func (m *testDBRepo) DeleteReservation(id int) error {
+	return nil
+}
+
+func (m *testDBRepo) UpdateProcessedForReservation(id int, processed int) error {
+	return nil
+}
